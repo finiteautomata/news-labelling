0db6ecfb1739f62bf7715d479248ead4b44d0951
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-09-07 11:54:38 -0300

    Embed comments in API

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for comment class
+    """
+    class Meta:
+        """
+        Meta class
+        """
+        model = Comment
+        fields = ['text', 'user_id', 'tweet_id', 'created_at']
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     """
     Serializer for User class
     """
+    comments = CommentSerializer(many=True, read_only=True, source='comment_set')
 
     class Meta:
+        """
+        Meta class
+        """
         model = Article
-        fields = ['title', 'tweet_id', 'text', 'url', 'user', 'body']
+        fields = ['title', 'tweet_id', 'text', 'url', 'user', 'body', 'comments', 'created_at']

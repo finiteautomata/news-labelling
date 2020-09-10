cc6f734f53c690fa2bd7595e3c1b52b2a2e6d075
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-09-10 16:17:22 -0300

    Basic assignment

---
+++ api/models/assignment.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+from .article import Article
+
+# Create your models here.
+class Assignment(models.Model):
+    """
+    Object linking user to articles
+
+
+    """
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def next_assignment_of(cls, user):
+        """
+        Returns next assignment of user
+        """
+        return cls.objects.filter(user=user, done=False).first()
+
+    def complete(self):
+        """
+        Set as done
+        """
+        if self.done:
+            # Raise if already completed
+            raise ValueError("Assignment already completed")
+        self.done = True
+        self.save()
---
+++ api/tests/assignments.py
@@ -0,0 +1,71 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from .factories import ArticleFactory
+from ..models import Assignment
+
+
+class AssignmentTest(TestCase):
+    """
+    Tests for validation and save of ArticleLabelSerializer
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test",
+            password="test",
+        )
+
+
+    def test_next_is_first_assignment(self):
+        """
+        Get first assignment
+        """
+        article = ArticleFactory()
+        Assignment.objects.create(user=self.user, article=article)
+
+        next_assignment = Assignment.next_assignment_of(self.user)
+        self.assertEqual(next_assignment.article, article)
+
+    def test_next_is_the_first_not_done(self):
+        """
+        Test we get the first not-done assignment
+        """
+        articles = ArticleFactory.create_batch(5)
+        assignments = [
+            Assignment.objects.create(user=self.user, article=article)
+            for article in articles
+        ]
+
+        assignments[0].complete()
+        assignments[1].complete()
+
+        next_assignment = Assignment.next_assignment_of(self.user)
+
+        self.assertEqual(next_assignment.article, articles[2])
+
+    def test_raise_if_no_next_assignment(self):
+        """
+        Test it raises if no next assignment
+        """
+        articles = ArticleFactory.create_batch(5)
+
+
+        for article in articles:
+            Assignment.objects.create(user=self.user, article=article, done=True)
+
+        assignment = Assignment.next_assignment_of(self.user)
+
+        self.assertEqual(assignment, None)
+
+
+    def test_raise_if_call_complete_twice(self):
+        """
+        Test it raises if no next assignment
+        """
+        article = ArticleFactory()
+        assignment = Assignment.objects.create(user=self.user, article=article)
+
+        assignment.complete()
+
+        with self.assertRaises(ValueError):
+            assignment.complete()
